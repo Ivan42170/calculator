fa7fce31707297dcfa6832d0e7eb7d59cd07c378
Ivan42170 <legocraft9968@gmail.com>
2020-08-29 13:40:14 +0200

    finish fonctionality

--- calculator.py
+++ calculator.py
@@ -42,13 +42,38 @@ def operation(action) :
     
 def equality() :
     second_number = float(entry_text.get())
-    entry_text.set(str(act([first_number, second_number])))
-
+    res = act([first_number, second_number])
+    if res == "" :
+        pass
+    elif res == int(res) :
+        res = int(res)
+    entry_text.set(str(res))
+    
 def mul(x) :
     c = 1
     for e in x :
         c = c*e
     return c
+
+def div(x) :
+    if x[1] != 0 :
+        c = x[0] / x[1]
+    else :
+        messagebox.showerror("Error", "division par 0")
+        c = ""
+    return c
+
+
+def minus(x) :
+    c = x[0] - x[1]
+    return c
+    
+def point() :
+    s = entry_text.get()
+    if s == "" or s == "-" or "." in s :
+        pass
+    else :
+        entry_text.set(entry_text.get()+".")
 
 b0 = Button(text="0", width=10, height=4, activebackground = '#0f62fb', bg = '#bdfdfb', command = lambda : insert("0"))
 #b0 = Button(text="0", width=10, height=4, activebackground='red', bg = '#cacaca', command=lambda x: )
@@ -62,13 +87,13 @@ b7 = Button(text="7", width=10, height=4, activebackground = '#0f62fb', bg = '#b
 b8 = Button(text="8", width=10, height=4, activebackground = '#0f62fb', bg = '#bdfdfb', command = lambda : entry_text.set(entry_text.get()+"8"))
 b9 = Button(text="9", width=10, height=4, activebackground = '#0f62fb', bg = '#bdfdfb', command = lambda : entry_text.set(entry_text.get()+"9"))
 b_plus = Button(text="+", width=10, height=4, bg = '#ffeda7', activebackground = '#fcf600', command = lambda : operation(sum))
-b_minus = Button(text="-", width=10, height=4, bg = '#ffeda7', activebackground = '#fcf600')
+b_minus = Button(text="-", width=10, height=4, bg = '#ffeda7', activebackground = '#fcf600', command = lambda : operation(minus))
 b_dot = Button(text="*", width=10, height=4, bg = '#ffeda7', activebackground = '#fcf600', command = lambda : operation(mul))
-b_dif = Button(text=":", width=10, height=4, bg = '#ffeda7', activebackground = '#fcf600')
+b_div = Button(text=":", width=10, height=4, bg = '#ffeda7', activebackground = '#fcf600', command = lambda : operation(div))
 b_del = Button(text="CE", width=10, height=4, bg = '#0feba3', activebackground = '#15d300', command = lambda : entry_text.set(""))
 b_eq = Button(text="=", width=10, height=4, bg = '#0feba3', activebackground = '#15d300', command = equality)
 b_sign = Button(text="+/-", width=10, height=4, bg = '#f98d27', activebackground = '#f94427', command = change_sign)
-b_mul = Button(text=',', width=10, height=4, bg = '#f98d27', activebackground ='#f94427', command = lambda : entry_text.set(entry_text.get()+"."))
+b_mul = Button(text=',', width=10, height=4, bg = '#f98d27', activebackground ='#f94427', command = point)
 
 top_space = 50
 button_width = 85
@@ -90,7 +115,7 @@ b9.place(x=left_space+button_width*2, y=top_space+button_height*0)
 b_plus.place(x=left_space+button_width*3, y=top_space+button_height*3)
 b_minus.place(x=left_space+button_width*3, y=top_space+button_height*2)
 b_del.place(x=left_space+button_width*3, y=top_space+button_height*0, width=2*button_width-5)
-b_dif.place(x=left_space+button_width*3, y=top_space+button_height*1)
+b_div.place(x=left_space+button_width*3, y=top_space+button_height*1)
 b_mul.place(x=left_space+button_width*0, y=top_space+button_height*3)
 b_sign.place(x=left_space+button_width*2, y=top_space+button_height*3)
 b_eq.place(x=left_space+button_width*4, y=top_space+button_height*2, height=2*button_height-5)
